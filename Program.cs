58a7236544fce3ddd08b456e3443c868ca056deb
FelipeAp1903 <131301360+FelipeAp1903@users.noreply.github.com>
2023-05-17 18:01:21 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lista3Ex3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double m = 109;
+            double f = 102;
+            double s;
+            double resultado = 0;
+
+            resultado = resultado + f;
+            
+            
+            {
+                Console.WriteLine("insira o sexo (F ou M):");
+                s = char.Parse(Console.ReadLine());
+                if(s==m)
+                    {
+                    Console.WriteLine("ok");
+                }
+                else
+                {
+                   
+                    
+                    
+                        if (s == f)
+                        {
+                            Console.WriteLine("ok");
+                        }
+                        else
+                        {
+
+
+                            Console.WriteLine("insira o sexo (F ou M):");
+                            s = char.Parse(Console.ReadLine());
+                        }
+                            
+                        
+                    
+                    
+                    
+                }
+                        
+               
+                    
+                    
+
+                    
+               
+
+            }
+
+
+           
+           
+
+
+
+
+
+
+
+           
+            
+
+
+
+
+
+
+
+
+               
+            
+
+
+
+        }
+    }
+}
